afe775535d3ad33a19c8702b5db48004a0fe6881
hwiseok0 <59162815+hwiseok0@users.noreply.github.com>
2019-12-31 16:28:50 +0900

    휘석 폼 추가

---
+++ Axxen/Axxen/HwiSeok/PRM_PRF_001.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace Axxen.HwiSeok
+{
+    partial class PRM_PRF_001
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // PRM_PRF_001
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "PRM_PRF_001";
+            this.Text = "PRM_PRF_001";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ Axxen/Axxen/HwiSeok/QAM_SQC_001.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Axxen.HwiSeok
+{
+    public partial class QAM_SQC_001 : Form
+    {
+        public QAM_SQC_001()
+        {
+            InitializeComponent();
+        }
+    }
+}
