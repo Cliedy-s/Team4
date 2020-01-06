d788d0c251310b62327731644647bd75b67a89c2
tkddls3319 <54829486+tkddls3319@users.noreply.github.com>
2020-01-06 15:24:28 +0900

    DAD

---
+++ Axxen/Axxen/ParkSangIn/MDS_SDS_006.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Axxen.ParkSangIn
+{
+    public partial class MDS_SDS_006 : Axxen.BaseForm
+    {
+        public MDS_SDS_006()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Axxen/Axxen/ParkSangIn/MSS_CON_002.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Axxen.ParkSangIn
+{
+    public partial class MSS_CON_002 : Axxen.BaseForm
+    {
+        public MSS_CON_002()
+        {
+            InitializeComponent();
+        }
+    }
+}
