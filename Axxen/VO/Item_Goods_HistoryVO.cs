bf6a2c9ce1f3c9256f3da2d0ba50eea6e7eaaaa8
sangyoung1 <59162269+sangyoung1@users.noreply.github.com>
2020-01-14 17:42:37 +0900

    sy

---
+++ Axxen/VO/Item_Goods_HistoryVO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VO
+{
+    class Item_Goods_HistoryVO //KPI_RPT_004
+    {
+        public string Item_Code { get; set; } //품목코드
+        public string Item_Name { get; set; } //품목명
+        public string In_Date { get; set; } //입고일자
+        public string In_Qty { get; set; } //생산수량
+    }
+}
