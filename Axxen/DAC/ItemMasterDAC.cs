b18c86c4f9d3fb7240401d0248011be50c03cf69
tkddls3319 <54829486+tkddls3319@users.noreply.github.com>
2020-01-20 18:14:42 +0900

    ㅇㅇ

---
+++ Axxen/DAC/ItemMasterDAC.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VO;
+
+namespace DAC
+{
+  public  class ItemMasterDAC : DACParent
+    {
+        /// <summary>
+        /// 품목그룹정보 전체
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="pwd"></param>
+        /// <returns></returns>
+        public List<Item_Level_MasterVO> GetAllItem_Level_Master()
+        {
+            List<Item_Level_MasterVO> ItemGroup = null;
+            using (SqlCommand com = new SqlCommand())
+            {
+                com.Connection = new SqlConnection(Connstr);
+                com.CommandText = "GetAllItem_Level_Master";
+                com.CommandType = CommandType.StoredProcedure;
+
+                com.Connection.Open();
+                SqlDataReader reader = com.ExecuteReader();
+                ItemGroup = Helper.DataReaderMapToList<Item_Level_MasterVO>(reader);
+                com.Connection.Close();
+
+                return ItemGroup;
+            }
+        }
+    }
+}
