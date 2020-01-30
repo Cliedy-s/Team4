df091109e8f67dba5b2cfc77b66bc2081679ceab
tkddls3319 <54829486+tkddls3319@users.noreply.github.com>
2020-01-30 20:47:30 +0900

    d

--- Axxen/Axxen/ParkSangIn/MDS_SDS_006_2.cs
+++ Axxen/Axxen/ParkSangIn/MDS_SDS_006_2.cs
@@ -20,8 +20,9 @@ namespace Axxen
         Inspect_Spec_MasterService inspectservice = new Inspect_Spec_MasterService();
         List<InspectSpecVO> inspectlist;
 
-
-        public string Code { get; set; }
+        CheckBox headerCheckBox = new CheckBox();
+        public string itemCode { get; set; }
+        public string inspectcode { get; set; }
         public MDS_SDS_006_2()
         {
             InitializeComponent();
@@ -61,12 +62,12 @@ namespace Axxen
 
                 inspectlist = new List<InspectSpecVO>();
                 inspectlist = inspectservice.GetAll();//품목규격전체
-                dgvinspect.DataSource = inspectlist.FindAll(item => item.Item_Code == Code);
+                dgvinspect.DataSource = inspectlist.FindAll(item => item.Item_Code == itemCode && item.Inspect_code == inspectcode);
             }
             catch (Exception err)
             {
                 Program.Log.WriteError(err.Message);
-                throw;
+                
             }
         }
         /// <summary>
@@ -74,18 +75,25 @@ namespace Axxen
         /// </summary>
         private void ControlSetting()
         {
-            DataGridViewButtonColumn gridbtn = new DataGridViewButtonColumn();
-            gridbtn.HeaderText = "사용여부";
-            gridbtn.Text = "변경";
-            gridbtn.Name = "btn";
-            gridbtn.Width = 100;
-            gridbtn.FlatStyle = FlatStyle.Popup;
-            gridbtn.DefaultCellStyle.BackColor = Color.LightYellow;
-            gridbtn.UseColumnTextForButtonValue = true;
-            dgvinspect.Columns.Add(gridbtn);
+
+            DataGridViewCheckBoxColumn gridcheckbox = new DataGridViewCheckBoxColumn();
+            gridcheckbox.HeaderText = "";
+            gridcheckbox.Name = "checkbtn";
+            gridcheckbox.Width = 30;
+            dgvinspect.Columns.Insert(0, gridcheckbox);
+
+
+
+            Point headerLocation = dgvinspect.GetCellDisplayRectangle(0, -1, true).Location;
+            headerCheckBox.Location = new Point(headerLocation.X + 8, headerLocation.Y + 2);
+            headerCheckBox.BackColor = Color.White;
+            headerCheckBox.Size = new Size(18, 18);
+            headerCheckBox.Click += new EventHandler(HeaderCheckBox_Click);
+            dgvinspect.Controls.Add(headerCheckBox);
+
 
             // combobox  품목명
-            Itemlist.RemoveAll(item => item.Item_Code ==Code);
+            Itemlist.RemoveAll(item => item.Item_Code ==itemCode);
             List<Item_MasterVO> dictionname = Itemlist.GroupBy(name => name.Item_Code).Select(grp => grp.First()).ToList();
             Dictionary<string, string> cbblistname = dictionname.ToDictionary(code => code.Item_Code, name => name.Item_Name);
             cbbItem.DisplayMember = "Value";
@@ -94,63 +102,60 @@ namespace Axxen
             lblItem.Text = cbbItem.SelectedValue.ToString();
         }
 
+        private void HeaderCheckBox_Click(object sender, EventArgs e)
+        {
+            dgvinspect.EndEdit();
+
+            foreach (DataGridViewRow row in dgvinspect.Rows)
+            {
+                DataGridViewCheckBoxCell chkBox = row.Cells["checkbtn"] as DataGridViewCheckBoxCell;
+                chkBox.Value = headerCheckBox.Checked;
+            }
+        }
+
         private void Dgvinspect_CellClick(object sender, DataGridViewCellEventArgs e)
         {
-            try
-            {
-                if (e.ColumnIndex == dgvinspect.Columns["btn"].Index)//눌러서 사용과 사용안함 변경
-                {
-                    if ((dgvinspect.SelectedRows[0].Cells[9].Value).ToString() == "Y") //사용안함
-                    {
-                        dgvinspect.SelectedRows[0].Cells[9].Value = "N";
-                    }
-                    else //사용함
-                    {
-                        dgvinspect.SelectedRows[0].Cells[9].Value = "Y";
-                    }          
-                }
-            }
-            catch (Exception err)
-            {
-
-                MessageBox.Show(err.Message);
-            }
+          
         }
 
         private void BtnSave_Click(object sender, EventArgs e)
         {
+            List<InspectSpecVO> addlist = new List<InspectSpecVO>();
             try
             {
                 Inspect_Spec_MasterService service = new Inspect_Spec_MasterService();
 
                 foreach (DataGridViewRow row in dgvinspect.Rows)
                 {
-                    if ((row.Cells[9].Value).ToString() == "Y")
-                    {                       
+                    if (Convert.ToBoolean(row.Cells[0].Value) == true) //체크박스가 선택된 row들만
+                    {
                         InspectSpecVO inspect = new InspectSpecVO {
 
                             Item_Code = lblItem.Text,
-                            Process_code = row.Cells[0].Value.ToString(),
-                            Inspect_code = row.Cells[1].Value.ToString(),
-                            Inspect_name = row.Cells[2].Value.ToString(),
+                            Process_code = row.Cells[1].Value.ToString(),
+                            Inspect_code = row.Cells[2].Value.ToString(),
+                            Inspect_name = row.Cells[3].Value.ToString(),
                             //Spec_Desc    =                     ,
-                            USL = Convert.ToInt32(row.Cells[3].Value),
-                            SL = Convert.ToInt32(row.Cells[4].Value),
-                            LSL = Convert.ToInt32(row.Cells[5].Value),
-                            Sample_size = Convert.ToInt32(row.Cells[6].Value),
-                            Inspect_Unit = row.Cells[7].Value.ToString(),
-                            Remark = row.Cells[8].Value.ToString()
+                            USL = Convert.ToInt32(row.Cells[4].Value),
+                            SL = Convert.ToInt32(row.Cells[5].Value),
+                            LSL = Convert.ToInt32(row.Cells[6].Value),
+                            Sample_size = Convert.ToInt32(row.Cells[7].Value),
+                            Inspect_Unit = row.Cells[8].Value.ToString(),
+                            Remark = row.Cells[9].Value.ToString()
                         };
 
-                        if (service.InsertInspectSpec(inspect))
-                        {
-                            MessageBox.Show("저장 완료", "알림", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                        }
-                        else
-                        {
-                            MessageBox.Show("이미 등록된 검사항목입니다.", "알림", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                        }
+                        addlist.Add(inspect);
+
+                     
                     }
+                }
+                if (service.InsertInspectSpec(inspect))
+                {
+                    MessageBox.Show("저장 완료", "알림", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("이미 등록된 검사항목입니다.", "알림", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 }
             }
             catch (Exception err)
